65843c1761973ac28a0cad2889443e3771ffac64
Oleg Shlyazhko <olmer@yandex-team.ru>
2015-08-03 15:23:29 +0300

    Базовые либы для js и python

---
+++ botan.js
@@ -0,0 +1,30 @@
+var request = require("request");
+
+var url = 'http://olmer.haze.yandex.net/botan?token=#token#&uid=#uid#&name=#name#';
+
+module.exports = function(apikey) {
+    token = apikey;
+    return {
+        track: function(message, name) {
+            uid = message.from.id;
+            name = name ? name : 'Message';
+            url = url.replace('#token#', token).replace('#uid#', uid).replace('#name#', name);
+            //console.log(url);
+            request({
+                    url: url,
+                    method: "POST",
+                    json: message
+                }, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        //console.log(body);
+                    }
+                    else {
+                        console.log("error: " + error);
+                        console.log("response.statusCode: " + response.statusCode);
+                        console.log("response.statusText: " + response.statusText);
+                    }
+                }
+            );
+        }
+    };
+};
---
+++ botan.py
@@ -0,0 +1,29 @@
+#----------------
+#dont forget 'pip install requests' first
+#----------------
+#usage example:
+#
+#import botan
+#
+#print botan.track(1111, 1, {'text':2}, 'Search')
+#
+
+import requests
+import json
+
+url_template = 'http://olmer.haze.yandex.net/botan?token=#token#&uid=#uid#&name=#name#';
+
+def track(token, uid, message, name = 'Message'):
+    global url_template
+    url = url_template.replace('#token#', str(token)).replace('#uid#', str(uid)).replace('#name#', name);
+    headers = {'Content-type': 'application/json'}
+    try:
+        r = requests.post(url, data=json.dumps(message), headers=headers)
+        return r.json()
+    except requests.exceptions.Timeout:
+        # set up for a retry, or continue in a retry loop
+        return False
+    except requests.exceptions.RequestException as e:
+        # catastrophic error
+        print e
+        return False
