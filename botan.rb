92ea0d4c3a8cc0bdcc2acfac7ec39d594204384a
Nikita Ilyasov <ilyasov@yandex-team.ru>
2015-08-03 16:59:38 +0300

    add ruby client

---
+++ botan.rb
@@ -0,0 +1,31 @@
+#usage example:
+#
+#require botan
+#
+#puts Botan.track(1111, 1, {'text' => 2}, 'Search')
+#
+module Botan
+  require 'net/http'
+  require 'net/https'
+  require 'json'
+
+  URI_TEMPLATE = 'http://retention.yurich.serp.yandex.ru/botan?token=%{token}&uid=%{uid}&name=%{name}';
+
+  def self.track(token, uid, message, name = 'Message')
+    begin
+      uri = URI(URI_TEMPLATE % {token: token, uid: uid, name: name})
+      puts uri
+      http = Net::HTTP.new(uri.host, uri.port)
+      body = JSON.dump(message)
+
+      req =  Net::HTTP::Post.new(uri)
+      req.add_field "Content-type", "application/json"
+      req.body = body
+      # Fetch Request
+      res = http.request(req)
+      return JSON.parse(res.body)
+    rescue StandardError => e
+      puts "HTTP Request failed (#{e.message})"
+    end
+  end
+end
