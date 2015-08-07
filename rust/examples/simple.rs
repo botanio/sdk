16924953be93a60b14a191ed5fe59f7eaa98b6a7
Fedor Gogolev <knsd@knsd.net>
2015-08-07 21:05:09 +0300

    Add rust library

---
+++ rust/examples/simple.rs
@@ -0,0 +1,21 @@
+extern crate rustc_serialize;
+
+extern crate botanio;
+
+use botanio::{Botan};
+
+#[derive(Debug, RustcEncodable)]
+struct Message {
+    some_metric: u32,
+    another_metric: u32,
+}
+
+fn main() {
+    let token = "1111";
+    let uid = 1;
+    let name = "Search";
+    let message = Message {some_metric: 100, another_metric: 500};
+
+    let botan = Botan::new(token);
+    botan.track(uid, &message, name).unwrap();
+}
---
+++ rust/src/lib.rs
@@ -0,0 +1,82 @@
+extern crate hyper;
+extern crate url;
+extern crate rustc_serialize;
+
+use std::io::{Read};
+
+use hyper::{Url};
+use hyper::client::{Client, IntoUrl};
+use hyper::header::ContentType;
+use hyper::error::Error as HyperError;
+use rustc_serialize::{Encodable};
+use rustc_serialize::json::{encode, decode};
+use url::ParseError as UrlError;
+
+const URL: &'static str = "https://api.botan.io/track";
+
+#[derive(Debug)]
+pub struct Botan {
+    url: Url,
+    token: String,
+}
+
+#[derive(Debug)]
+pub enum BotanError {
+    HttpError(HyperError),
+    JsonError,
+    Failed,
+    BadRequest,
+    Unknown,
+}
+
+#[derive(Debug, RustcDecodable)]
+struct BotanResponse {
+    status: String,
+}
+
+impl Botan {
+
+    pub fn new(token: &str) -> Botan {
+        Botan { url: URL.into_url().unwrap(), token: token.to_string() }
+    }
+
+    pub fn set_url<U: IntoUrl>(mut self, url: U) -> Result<Botan, UrlError> {
+        self.url = try!(url.into_url());
+        Ok(self)
+    }
+
+    pub fn track<M: Encodable>(&self, uid: u64, message: &M, name: &str) -> Result<(), BotanError> {
+        let mut url = self.url.clone();
+        let s_uid = &uid.to_string();
+        let query_pairs: Vec<(&str, &str)> = vec![
+            ("token", &self.token),
+            ("uid", s_uid),
+            ("name", name),
+        ];
+
+        url.set_query_from_pairs(query_pairs.into_iter());
+
+        let body = try!(encode(message).map_err(|_| BotanError::JsonError));
+        let client = Client::new();
+
+        let mut response_string = String::new();
+        try!(client.post(url)
+        .header(ContentType::json())
+        .body(&body)
+        .send()
+        .map_err(BotanError::HttpError)
+        .and_then(|mut resp| resp.read_to_string(&mut response_string).map_err(|err| BotanError::HttpError(HyperError::Io(err)))));
+
+        match try!(decode::<BotanResponse>(&response_string).map_err(|_| BotanError::JsonError)).status.as_ref() {
+            "accepted"      => Ok(()),
+            "failed"        => Err(BotanError::Failed),
+            "bad request"   => Err(BotanError::BadRequest),
+            _               => Err(BotanError::Unknown),
+        }
+    }
+}
+
+#[test]
+fn test_is_default_url_correct() {
+    assert!(URL.into_url().is_ok())
+}
