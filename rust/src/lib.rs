da498bac21e4c94ae3ac8cce31dbad6d8ed79fbe
Fedor Gogolev <knsd@knsd.net>
2015-11-22 20:04:56 +0300

    Rust module documentation

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,16 @@
+//!
+//! Botan is a telegram bot analytics system based on Yandex.Appmetrica.
+//!
+//! # Creating an account
+//! Register at http://appmetrica.yandex.com/.
+//!
+//! After registration you will be prompted to create Application. Please use @YourBotName as a name.
+//!
+//! Save an API key from settings page, you will use it as a token for `Botan` initalization.
+//!
+//! For more information see examples.
+//!
+
 extern crate hyper;
 extern crate url;
 extern crate rustc_serialize;
@@ -14,6 +27,7 @@ use rustc_serialize::json::{encode, decode};
 
 const URL: &'static str = "https://api.botan.io/track";
 
+/// An entry point to the botan.io API
 #[derive(Debug)]
 pub struct Botan {
     url: Url,
@@ -21,12 +35,19 @@ pub struct Botan {
 }
 
 #[derive(Debug)]
+/// This error could occure during tracking
 pub enum BotanError {
+    /// Underlying http error, dns error for example
     Http(HyperError),
+    /// Impossible to encode a message
     JsonEncoding,
+    /// Impossible to decode answer from the server which provided in field
     JsonDecoding(String),
+    /// Server is unable to process request
     Failed,
+    /// Malformed request, additional info is provided in field
     BadRequest(String),
+    /// Invalid status, or "info" field with "accepted" or "failed" status
     Unknown,
 }
 
@@ -44,15 +65,24 @@ struct BotanRequest {
 
 impl Botan {
 
+    /// Creates a new entry point to Botan API
     pub fn new(token: &str) -> Botan {
         Botan { url: URL.into_url().unwrap(), token: token.to_string() }
     }
 
+    /// Sets custom url to botan.io api server
     pub fn set_url<U: IntoUrl>(mut self, url: U) -> Result<Botan, UrlError> {
         self.url = try!(url.into_url());
         Ok(self)
     }
 
+    /// Send tracking information associated with user
+    ///
+    /// Parameters:
+    ///
+    ///   * uid: Supposed to be a telegram user id
+    ///   * name: Event name
+    ///   * message: Any object that can be encoded to json
     pub fn track<M: Encodable>(&self, uid: u32, name: &str, message: &M) -> Result<(), BotanError> {
         let request = try!(self.prepare_request(uid, name, message));
         let response = try!(Self::make_request(request));
