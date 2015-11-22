d3a1683733e21b017fc0a88c29dce4325ea66e0a
Fedor Gogolev <knsd@knsd.net>
2015-11-22 17:19:05 +0300

    Better error handling

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -23,15 +23,23 @@ pub struct Botan {
 #[derive(Debug)]
 pub enum BotanError {
     Http(HyperError),
-    Json,
+    JsonEncoding,
+    JsonDecoding(String),
     Failed,
-    BadRequest,
+    BadRequest(String),
     Unknown,
 }
 
 #[derive(Debug, RustcDecodable)]
 struct BotanResponse {
     status: String,
+    info: Option<String>
+}
+
+#[derive(Debug)]
+struct BotanRequest {
+    url: Url,
+    body: String,
 }
 
 impl Botan {
@@ -46,9 +54,15 @@ impl Botan {
     }
 
     pub fn track<M: Encodable>(&self, uid: u32, message: &M, name: &str) -> Result<(), BotanError> {
+        let request = try!(self.prepare_request(uid, message, name));
+        let response = try!(Self::make_request(request));
+        Self::treat_response(response)
+    }
+
+    fn prepare_request<M: Encodable>(&self, uid: u32, message: &M, name: &str) -> Result<BotanRequest, BotanError> {
         let mut url = self.url.clone();
         let s_uid = &uid.to_string();
-        let query_pairs: Vec<(&str, &str)> = vec![
+        let query_pairs: [(&str, &str); 3] = [
             ("token", &self.token),
             ("uid", s_uid),
             ("name", name),
@@ -56,22 +70,44 @@ impl Botan {
 
         url.set_query_from_pairs(query_pairs.into_iter());
 
-        let body = try!(encode(message).map_err(|_| BotanError::Json));
+        Ok(BotanRequest {
+            url: url,
+            body: try!(encode(message).map_err(|_| BotanError::JsonEncoding)),
+        })
+    }
+
+    fn make_request(request: BotanRequest) -> Result<BotanResponse, BotanError> {
         let client = Client::new();
 
         let mut response_string = String::new();
-        try!(client.post(url)
-        .header(ContentType::json())
-        .body(&body)
-        .send()
-        .map_err(BotanError::Http)
-        .and_then(|mut resp| resp.read_to_string(&mut response_string).map_err(|err| BotanError::Http(HyperError::Io(err)))));
+        try!(
+            client.post(request.url)
+                  .header(ContentType::json())
+                  .body(&request.body)
+                  .send()
+                  .map_err(BotanError::Http)
+                  .and_then(|mut resp| {
+                resp.read_to_string(&mut response_string)
+                    .map_err(|err| BotanError::Http(HyperError::Io(err)))
+            })
+        );
+        decode(&response_string).map_err(|_| BotanError::JsonDecoding(response_string))
 
-        match try!(decode::<BotanResponse>(&response_string).map_err(|_| BotanError::Json)).status.as_ref() {
-            "accepted"      => Ok(()),
-            "failed"        => Err(BotanError::Failed),
-            "bad request"   => Err(BotanError::BadRequest),
-            _               => Err(BotanError::Unknown),
+    }
+
+    fn treat_response(response: BotanResponse) -> Result<(), BotanError> {
+        match response {
+            BotanResponse { ref status, info: None } =>
+                match status.as_ref() {
+                    "accepted" => Ok(()),
+                    "failed"   => Err(BotanError::Failed),
+                    _          => Err(BotanError::Unknown),
+                },
+            BotanResponse { ref status, info: Some(ref info) } =>
+                match status.as_ref() {
+                    "bad request" => Err(BotanError::BadRequest(info.to_string())),
+                    _             => Err(BotanError::Unknown),
+                }
         }
     }
 }
