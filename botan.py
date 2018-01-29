f4a0229df7d501b66c8203c6be637885791963ac
ihoru <i.h.o.r.p.o.l.y.a.k.o.v@gmail.com>
2018-01-29 19:36:54 +0700

    logging instead print & ability to pass addition params to requests.post

--- botan.py
+++ botan.py
@@ -7,29 +7,34 @@
 #
 # print botan.track(1111, 1, {'text':2}, 'Search')
 
-import requests
 import json
+import logging
 
+import requests
+
+logger = logging.getLogger(__name__)
 
 TRACK_URL = 'https://api.botan.io/track'
 SHORTENER_URL = 'https://api.botan.io/s/'
 
 
-def track(token, uid, message, name='Message'):
+def track(token, uid, message, name='Message', **request_kwargs):
     try:
         r = requests.post(
             TRACK_URL,
             params={"token": token, "uid": uid, "name": name},
             data=json.dumps(message),
             headers={'Content-type': 'application/json'},
+            **request_kwargs,
         )
         return r.json()
-    except requests.exceptions.Timeout:
+    except requests.exceptions.Timeout as e:
         # set up for a retry, or continue in a retry loop
+        logger.exception(e)
         return False
     except (requests.exceptions.RequestException, ValueError) as e:
         # catastrophic error
-        print(e)
+        logger.exception(e)
         return False
 
 
